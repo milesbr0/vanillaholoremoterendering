3c254245511d2519101830199ec2bdb04a14f6fa
AzureAD\RichárdKiss <richard.kiss@360.world>
2020-08-25 11:27:53 +0200

    Unity project

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour { 
+
+    public Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 15f * Time.deltaTime, Space.World);
+        renderer.material.color = new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256),255);
+    }
+}
